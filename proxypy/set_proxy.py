99dfc4d35eaf413de989fdd1e204bce5a0a962d1
David Newton <dnewton@ford.com>
2022-10-18 10:20:43 +0200

    tidy

--- proxypy/set_proxy.py
+++ proxypy/set_proxy.py
@@ -1,11 +1,15 @@
 import socket
 import os
+import yaml 
 
 try:
   s=socket.getaddrinfo('www.google.com', 80)
   print('no proxy set')
 except:
-  os.environ["http_proxy"] = 'http://internet.ford.com:83'
-  os.environ["https_proxy"] = 'http://internet.ford.com:83'
-  os.environ["no_proxy"] = 'localhost,127.0.0.1,.ford.com'
+  with open('proxypy/security_rules.yaml') as file:
+        config_data_input = yaml.load(file, Loader=yaml.FullLoader)
+  proxy = config_data_input.get('proxy')
+  os.environ["http_proxy"] = proxy.get('http_proxy')
+  os.environ["https_proxy"] = proxy.get('https_proxy')
+  os.environ["no_proxy"] = proxy.get('no_proxy')
   print('proxy set')
